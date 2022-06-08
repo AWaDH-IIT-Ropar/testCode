e946a6acc9dca72867755b39140cac90371be498
Pratyush-Ver <pratyushverma.iitrpr@gmail.com>
2022-06-08 15:21:33 +0530

    ranacore june 6 version

--- rana/rana.sh
+++ rana/rana.sh
@@ -13,7 +13,7 @@ stamp() {
 while :
 do
     var=`stamp`
-    ffmpeg -f v4l2 -framerate 60 -r 60 -video_size 640x480 -i /dev/video0 -t 00:00:30 -q:v 8 -f MJPEG pipe:1 | ./ranacore -c ranacore.conf | ffmpeg -i - -q:v 2 -vcodec copy ${var}.avi
+    ffmpeg -f v4l2 -framerate 60 -r 60 -video_size 640x480 -i /dev/video2 -t 00:00:30 -q:v 5 -f MJPEG pipe:1 | ./ranacore -c ranacore.conf | ffmpeg -i - -q:v 2 -vcodec copy /media/mmcblk1p1/upload/${var}.mjpg
     systemd-notify WATCHDOG=1
     sleep 5
 done
