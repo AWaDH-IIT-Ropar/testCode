88bc4f45dc3746950bf476610e21f14940521a4f
Pratyush-Ver <pratyushverma.iitrpr@gmail.com>
2022-08-30 17:20:34 +0530

    update.sh modified

--- update.sh
+++ update.sh
@@ -15,12 +15,18 @@
 # fi
 
 cd /usr/sbin/device-manager/DeviceManager/templates/
+rm configuration.html.backup
 mv configurations.html configuration.html.backup 
 wget https://raw.githubusercontent.com/AWaDH-IIT-Ropar/testCode/atul/DeviceManager/templates/configurations.html
+rm videoFeed.html.backup
+mv videoFeed.html videoFeed.html.backup 
 wget https://raw.githubusercontent.com/AWaDH-IIT-Ropar/testCode/atul/DeviceManager/templates/videoFeed.html
 cd /usr/sbin/device-manager/DeviceManager/
+rm app.py.backup
 mv app.py app.py.backup
 wget https://raw.githubusercontent.com/AWaDH-IIT-Ropar/testCode/atul/DeviceManager/app.py
 cd /usr/sbin/device-manager/DeviceManager/static/css/
+rm videoFeed.css.backup
+mv videoFeed.css videoFeed.css.backup
 wget https://raw.githubusercontent.com/AWaDH-IIT-Ropar/testCode/atul/DeviceManager/static/css/videoFeed.css
 
